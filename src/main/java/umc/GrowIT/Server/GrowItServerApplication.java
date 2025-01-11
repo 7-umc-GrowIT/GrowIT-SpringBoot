0da48924898353a4f1fb5cbdc7c68551e1bb72c0
Jinseong <jinsung528@gmail.com>
2025-01-11 12:30:23 +0900

    SpringBoot 프로젝트 세팅

---
+++ src/main/java/umc/GrowIT/Server/GrowItServerApplication.java
@@ -0,0 +1,13 @@
+package umc.GrowIT.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowItServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowItServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/umc/GrowIT/Server/GrowItServerApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.GrowIT.Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowItServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
