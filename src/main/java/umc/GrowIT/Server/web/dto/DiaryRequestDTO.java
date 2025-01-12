6c4381b95dad99b9524bac27515ad1b6dc9c12f7
Santoragi <woojj1254577@naver.com>
2025-01-12 15:30:05 +0900

    [#16] Feat: 일기 관련 API 명세서 작성

---
+++ src/main/java/umc/GrowIT/Server/web/dto/DiaryRequestDTO.java
@@ -0,0 +1,20 @@
+package umc.GrowIT.Server.web.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+public class DiaryRequestDTO {
+    @Getter
+    @Setter
+    public static class DiaryDTO {
+        @NotNull
+        String title;
+        @NotNull
+        String content;
+        @NotNull
+        LocalDate date;
+    }
+}
