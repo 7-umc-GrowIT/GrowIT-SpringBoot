a1a305dd22807899c44c8629cb65343f05ca021d
syeongk <syeongggg@gmail.com>
2025-01-18 01:48:01 +0900

    [#51] Feat: 회원가입 API 이메일 인증 확인

--- src/main/java/umc/GrowIT/Server/web/dto/UserDTO/UserRequestDTO.java
+++ src/main/java/umc/GrowIT/Server/web/dto/UserDTO/UserRequestDTO.java
@@ -20,6 +20,7 @@ public class UserRequestDTO {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class UserInfoDTO {
+        private Boolean isVerified;
 
         @NotBlank(message = "필수 입력 항목입니다.")
         @Email(message = "이메일 형식에 맞춰주세요.")
