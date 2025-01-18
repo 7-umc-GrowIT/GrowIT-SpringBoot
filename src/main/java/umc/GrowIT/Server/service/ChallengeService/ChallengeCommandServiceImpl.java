ccbfd436a64f97cdea2790cedd2b75147686b7fa
Jinseong <jinsung528@gmail.com>
2025-01-18 23:25:34 +0900

    [#49] Feat: 챌린지 삭제 API 개발

--- src/main/java/umc/GrowIT/Server/service/ChallengeService/ChallengeCommandServiceImpl.java
+++ src/main/java/umc/GrowIT/Server/service/ChallengeService/ChallengeCommandServiceImpl.java
@@ -1,15 +1,23 @@
 package umc.GrowIT.Server.service.ChallengeService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import umc.GrowIT.Server.apiPayload.code.status.ErrorStatus;
+import umc.GrowIT.Server.apiPayload.exception.ChallengeHandler;
+import umc.GrowIT.Server.converter.ChallengeConverter;
 import umc.GrowIT.Server.domain.Challenge;
+import umc.GrowIT.Server.domain.UserChallenge;
 import umc.GrowIT.Server.repository.ChallengeRepository;
+import umc.GrowIT.Server.repository.UserChallengeRepository;
+import umc.GrowIT.Server.web.dto.ChallengeDTO.ChallengeResponseDTO;
 
 @Service
 @RequiredArgsConstructor
 public class ChallengeCommandServiceImpl implements ChallengeCommandService {
 
     private final ChallengeRepository challengeRepository;
+    private final UserChallengeRepository userChallengeRepository;
 
     @Override
     public void markChallengeAsCompleted(Long userId, Long challengeId) {
@@ -21,5 +29,24 @@ public class ChallengeCommandServiceImpl implements ChallengeCommandService {
 
         // 변경된 챌린지 저장
         challengeRepository.save(challenge);
+    }
+
+    @Override
+    public ChallengeResponseDTO.DeleteChallengeResponseDTO delete(Long userChallengeId, Long userId) {
+
+        // 1. userId와 userChallengeId를 통해 조회하고 없으면 오류
+        UserChallenge userChallenge = userChallengeRepository.findByIdAndUserId(userChallengeId, userId)
+                .orElseThrow(() -> new ChallengeHandler(ErrorStatus.USER_CHALLENGE_NOT_FOUND));
+
+        // 2. 진행 중(false)인 챌린지인지 체크, 완료(true)한 챌린지면 오류
+        if(userChallenge.isCompleted()) {
+            throw new ChallengeHandler(ErrorStatus.USER_CHALLENGE_COMPLETE);
+        }
+
+        // 3. 삭제
+        userChallengeRepository.deleteById(userChallengeId);
+
+        // 4. converter 작업
+        return ChallengeConverter.toDeletedUserChallenge(userChallenge);
     }
 }
