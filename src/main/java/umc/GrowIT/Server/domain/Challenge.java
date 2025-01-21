2c17990f538fb5731f06528a7ecbafb35cf797c9
벨라 <icomksh@gmail.com>
2025-01-21 10:53:26 +0900

    Refactor: 현황 조회 수정

--- src/main/java/umc/GrowIT/Server/domain/Challenge.java
+++ src/main/java/umc/GrowIT/Server/domain/Challenge.java
@@ -61,11 +61,9 @@ public class Challenge extends BaseEntity {
 
     public String getStatus() {
         if (this.dtype != null) {
-            return this.dtype.name(); // dtype 값 반환 (RANDOM, DAILY)
+            return this.completed ? this.dtype.name() + "_COMPLETED" : this.dtype.name(); // 완료하였으면 뒤에 _COMPLETED 추가하여 반환
         }
-        if (this.completed) {
-            return "COMPLETED"; // completed가 true인 경우
-        }
-        return "TOTAL"; // completed가 false인 경우
+        return this.completed ? "COMPLETED" : "TOTAL";
     }
+
 }
