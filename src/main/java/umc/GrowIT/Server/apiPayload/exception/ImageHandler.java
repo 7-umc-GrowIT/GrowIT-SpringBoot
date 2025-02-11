18e1b549f6aca15fe9664ea0285dafbd22bb2dda
벨라 <icomksh@gmail.com>
2025-02-11 12:55:51 +0900

    Image 관련 파일 삭제

--- src/main/java/umc/GrowIT/Server/apiPayload/exception/ImageHandler.java
+++
@@ -1,9 +0,0 @@
-package umc.GrowIT.Server.apiPayload.exception;
-
-import umc.GrowIT.Server.apiPayload.code.BaseErrorCode;
-
-public class ImageHandler extends GeneralException {
-    public ImageHandler(BaseErrorCode code) {
-        super(code);
-    }
-}
