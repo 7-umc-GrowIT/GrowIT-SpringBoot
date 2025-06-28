009dcb91c6b761461ebb3577c22b90020ff9b843
HSSJW <okqkfkfk@naver.com>
2025-06-28 18:37:25 +0900

    [#240] Feat : 크레딧 결제 컨트롤러 구현

--- src/main/java/umc/GrowIT/Server/web/controller/specification/CreditProductSpecification.java
+++ src/main/java/umc/GrowIT/Server/web/controller/specification/CreditProductSpecification.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import umc.GrowIT.Server.apiPayload.ApiResponse;
 import umc.GrowIT.Server.web.dto.CreditProductDTO.CreditProductResponseDTO;
-import umc.GrowIT.Server.web.dto.PaymentDTO.PaymentRequestDTO;
-import umc.GrowIT.Server.web.dto.PaymentDTO.PaymentResponseDTO;
+import umc.GrowIT.Server.web.dto.CreditPaymentDTO.CreditPaymentRequestDTO;
+import umc.GrowIT.Server.web.dto.CreditPaymentDTO.CreditPaymentResponseDTO;
 
 public interface CreditProductSpecification {
 
@@ -18,10 +18,10 @@ public interface CreditProductSpecification {
     public ApiResponse<CreditProductResponseDTO.CreditProductListDTO> getCreditProductList();
 
 
-    @Operation(summary = "크레딧 구매", description = "결제를 통해 크레딧을 구매합니다.")
+    @Operation(summary = "크레딧 구매정보 처리", description = "결제를 통해 크레딧을 구매합니다.")
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "⭕ SUCCESS"),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "PAYMENT4001", description = "❌ 영수증 검증에 실패했습니다.")})
     @PostMapping("/credits/payment")
-    public ApiResponse<PaymentResponseDTO> purchaseCredits(@RequestBody PaymentRequestDTO request);
+    public ApiResponse<CreditPaymentResponseDTO> purchaseCredits(@RequestBody CreditPaymentRequestDTO request);
 }
