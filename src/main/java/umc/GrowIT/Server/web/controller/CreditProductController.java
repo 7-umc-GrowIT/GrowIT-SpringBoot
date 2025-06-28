39a9ea03c4e41bb316e4a4e62c9614a4b875930a
HSSJW <okqkfkfk@naver.com>
2025-06-28 18:37:16 +0900

    [#240] Feat : 크레딧 결제 컨트롤러 구현

--- src/main/java/umc/GrowIT/Server/web/controller/CreditProductController.java
+++ src/main/java/umc/GrowIT/Server/web/controller/CreditProductController.java
@@ -1,15 +1,17 @@
 package umc.GrowIT.Server.web.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import umc.GrowIT.Server.apiPayload.ApiResponse;
 import umc.GrowIT.Server.service.CreditProductService.CreditProductQueryService;
+import umc.GrowIT.Server.service.PaymentService.PaymentCommandService;
 import umc.GrowIT.Server.web.controller.specification.CreditProductSpecification;
 import umc.GrowIT.Server.web.dto.CreditProductDTO.CreditProductResponseDTO;
-import umc.GrowIT.Server.web.dto.PaymentDTO.PaymentRequestDTO;
-import umc.GrowIT.Server.web.dto.PaymentDTO.PaymentResponseDTO;
+import umc.GrowIT.Server.web.dto.CreditPaymentDTO.CreditPaymentRequestDTO;
+import umc.GrowIT.Server.web.dto.CreditPaymentDTO.CreditPaymentResponseDTO;
 
 @Tag(name = "CreditProduct", description = "크레딧 상품 API")
 @RestController
@@ -18,7 +20,7 @@ import umc.GrowIT.Server.web.dto.PaymentDTO.PaymentResponseDTO;
 public class CreditProductController implements CreditProductSpecification {
 
     private CreditProductQueryService creditProductQueryService;
-
+    private PaymentCommandService paymentCommandService;
 
     //활성화된 크레딧 상품 리스트 조회
     @GetMapping("/credits")
@@ -31,9 +33,14 @@ public class CreditProductController implements CreditProductSpecification {
     }
 
 
+    //크레딧 구매정보 처리
     @PostMapping("/credits/payment")
-    public ApiResponse<PaymentResponseDTO> purchaseCredits(@RequestBody PaymentRequestDTO request){
+    public ApiResponse<CreditPaymentResponseDTO> purchaseCredits(@RequestBody CreditPaymentRequestDTO request){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Long userId = (Long) authentication.getPrincipal(); // 사용자 인가정보
 
-        return null;
+        CreditPaymentResponseDTO result = paymentCommandService.processCreditPayment(userId, request);
+
+        return ApiResponse.onSuccess(result);
     }
 }
