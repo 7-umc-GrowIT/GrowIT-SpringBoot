e5f66ea5372b2c3d8af98608cd462091f1841f6e
HSSJW <okqkfkfk@naver.com>
2025-07-07 15:38:46 +0900

    [#240] Feat : 크레딧 결제 서비스 임시구현

--- src/main/java/umc/GrowIT/Server/service/PaymentService/PaymentCommandService.java
+++ src/main/java/umc/GrowIT/Server/service/PaymentService/PaymentCommandService.java
@@ -4,7 +4,9 @@ import umc.GrowIT.Server.web.dto.CreditPaymentDTO.CreditPaymentRequestDTO;
 import umc.GrowIT.Server.web.dto.CreditPaymentDTO.CreditPaymentResponseDTO;
 
 public interface PaymentCommandService {
-
-
+    
+    //크레딧 구매 처리
     public CreditPaymentResponseDTO processCreditPayment(Long userId, CreditPaymentRequestDTO requestDto);
+
+    public boolean validatePaymentRequest(CreditPaymentRequestDTO requestDto);
 }
--- src/main/java/umc/GrowIT/Server/service/PaymentService/PaymentCommandServiceImpl.java
+++ src/main/java/umc/GrowIT/Server/service/PaymentService/PaymentCommandServiceImpl.java
@@ -1,5 +1,28 @@
 package umc.GrowIT.Server.service.PaymentService;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import umc.GrowIT.Server.web.dto.CreditPaymentDTO.CreditPaymentRequestDTO;
+import umc.GrowIT.Server.web.dto.CreditPaymentDTO.CreditPaymentResponseDTO;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class PaymentCommandServiceImpl implements PaymentCommandService {
+
+    @Override // 크레딧 구매처리
+    public CreditPaymentResponseDTO processCreditPayment(Long userId, CreditPaymentRequestDTO requestDto){
+
+        return null;
+    }
+
+    @Override // apple store와 비교하여 검증
+    @Transactional(readOnly = true)
+    public boolean validatePaymentRequest(CreditPaymentRequestDTO requestDto){
+
+        return true;
+    }
 
 }
