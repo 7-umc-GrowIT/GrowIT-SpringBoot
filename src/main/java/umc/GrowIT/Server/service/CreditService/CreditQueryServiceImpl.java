9f1496762074f2e4520d25819a4425f01a144536
HSSJW <okqkfkfk@naver.com>
2025-07-23 16:22:25 +0900

    [#240] Refactor : 리포지토리 수정에 따른 클래스명 변경

--- src/main/java/umc/GrowIT/Server/service/CreditService/CreditQueryServiceImpl.java
+++ src/main/java/umc/GrowIT/Server/service/CreditService/CreditQueryServiceImpl.java
@@ -4,18 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import umc.GrowIT.Server.converter.CreditConverter;
-import umc.GrowIT.Server.domain.enums.ItemCategory;
-import umc.GrowIT.Server.repository.CreditRepository.CreditRepository;
+import umc.GrowIT.Server.repository.CreditRepository.UserCreditRepository;
 import umc.GrowIT.Server.web.dto.CreditDTO.CreditResponseDTO;
-import umc.GrowIT.Server.web.dto.ItemDTO.ItemResponseDTO;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class CreditQueryServiceImpl implements CreditQueryService {
-    private final CreditRepository creditRepository;
+    private final UserCreditRepository creditRepository;
 
     public CreditResponseDTO.CurrentCreditDTO getCurrentCredit(Long userId){
 
