f751c69aeaaac14479f0afb38d71fffbbc24feea
HSSJW <okqkfkfk@naver.com>
2025-07-23 15:26:14 +0900

    [#240] Feat : 크래딧 개수 업데이트 리포지토리 메서드 추가

--- src/main/java/umc/GrowIT/Server/repository/CreditRepository/UserCreditRepository.java
+++ src/main/java/umc/GrowIT/Server/repository/CreditRepository/UserCreditRepository.java
@@ -1,21 +1,22 @@
 package umc.GrowIT.Server.repository.CreditRepository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import umc.GrowIT.Server.domain.User;
 
 public interface CreditRepository extends JpaRepository<User, Long> {
 
-    // 현재 보유 크레딧 조회
-//    public Integer findCurrentCreditById(Long id);
-
     // 누적 크레딧 조회
 //    public Integer findTotalCreditById(Long id);
-
     @Query("SELECT u.currentCredit FROM User u WHERE u.id = :id")
     Integer findCurrentCreditById(@Param("id") Long id);
 
     @Query("SELECT u.totalCredit FROM User u WHERE u.id = :id")
     Integer findTotalCreditById(@Param("id") Long id);
+
+    @Modifying // 사용자의 크레딧 개수 업데이트
+    @Query("UPDATE User u SET u.currentCredit = u.currentCredit + :creditAmount, u.totalCredit = u.totalCredit + :creditAmount WHERE u.id = :userId")
+    void addCreditToUser(@Param("userId") Long userId, @Param("creditAmount") Integer creditAmount);
 }
