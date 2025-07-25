1ef745dfa7802dc9c6d83eba2d5e870106ec7645
HSSJW <okqkfkfk@naver.com>
2025-07-25 14:58:37 +0900

    [#240] Comment : 주석 추가

--- src/main/java/umc/GrowIT/Server/config/RestTemplateConfig.java
+++ src/main/java/umc/GrowIT/Server/config/RestTemplateConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-//AppStore의 영수증 검증 api호출을 위해 spring이 HTTP 요청을 보내기 위한 Bean 설정
+//AppStore의 영수증 검증 api호출을 위해 spring이 HTTP 요청을 보내기 위한 Bean 생성(서버 시작 시)
 @Configuration
 public class RestTemplateConfig {
     
