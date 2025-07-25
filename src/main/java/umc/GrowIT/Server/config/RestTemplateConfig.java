17690627559c2ba5fea02a6ef1a8d1738e622a5c
HSSJW <okqkfkfk@naver.com>
2025-07-25 14:56:29 +0900

    [#240] Feat : Spring HTTP요청 객체 생성

--- src/main/java/umc/GrowIT/Server/config/RestTemplateConfig.java
+++ src/main/java/umc/GrowIT/Server/config/RestTemplateConfig.java
@@ -1,12 +1,20 @@
 package umc.GrowIT.Server.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+//AppStore의 영수증 검증 api호출을 위해 spring이 HTTP 요청을 보내기 위한 Bean 설정
 @Configuration
 public class RestTemplateConfig {
-
+    
+    @Bean
     public RestTemplate restTemplate() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(10000); // 연결 타임아웃 10초
+        factory.setReadTimeout(30000);    // 일기 타임아웃 30초
+
         return new RestTemplate();
     }
 
