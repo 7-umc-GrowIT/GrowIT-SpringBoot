e91ccebb50655025549d509c8a01a8ba7a27846d
HSSJW <okqkfkfk@naver.com>
2025-07-25 14:56:50 +0900

    [#240] Comment : 주석 추가

--- src/main/java/umc/GrowIT/Server/domain/enums/PaymentStatus.java
+++ src/main/java/umc/GrowIT/Server/domain/enums/PaymentStatus.java
@@ -2,4 +2,5 @@ package umc.GrowIT.Server.domain.enums;
 
 public enum PaymentStatus {
     SUCCESS, FAILED, PENDING, CANCELLED, REFUNDED, VALIDATION_FAILED
+    //성공     실패    진행 중    취소완료    환불완료      연수증 검사 실패
 }
