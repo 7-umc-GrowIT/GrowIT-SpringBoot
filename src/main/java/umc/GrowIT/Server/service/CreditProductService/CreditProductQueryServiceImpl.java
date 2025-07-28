c7ef6ff69bc6c59fc92cb9ce1663a51ef736e71c
HSSJW <okqkfkfk@naver.com>
2025-07-28 17:21:51 +0900

    [#240] Refactor : 불필요한 메서드 제거

--- src/main/java/umc/GrowIT/Server/service/CreditProductService/CreditProductQueryServiceImpl.java
+++ src/main/java/umc/GrowIT/Server/service/CreditProductService/CreditProductQueryServiceImpl.java
@@ -3,7 +3,6 @@ package umc.GrowIT.Server.service.CreditProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import umc.GrowIT.Server.converter.CreditProductConverter;
 import umc.GrowIT.Server.domain.CreditProduct;
 import umc.GrowIT.Server.repository.CreditProductRepository;
@@ -14,7 +13,6 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-@Transactional(readOnly = true)
 public class CreditProductQueryServiceImpl implements CreditProductQueryService {
 
     private final CreditProductRepository creditProductRepository;
@@ -27,22 +25,4 @@ public class CreditProductQueryServiceImpl implements CreditProductQueryService 
         return CreditProductConverter.creditProductListDTO(creditProducts);
     }
 
-    @Override
-    public boolean validateProduct(String productId, Integer creditAmount, Integer paymentAmount){
-
-        log.info("상품 검증 시작: productId={}, creditAmount={}, paymentAmount={}",
-                productId, creditAmount, paymentAmount);
-
-        // 1. 상품 존재 여부 확인
-
-        // 2. 상품 활성화 여부 확인
-
-        // 3. 크레딧 수량 검증  (클라이언트로 부터 requestBody로 전달받은 값들 검증 -> DB의 해당 productId 상품의 가격&크레딧 수량 일치하는지 검증)
-
-        // 4. 결제 금액 검증
-
-
-
-        return true;
-    }
 }
