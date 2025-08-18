7305f7760f5bda91671cbaffcd68cf8f62ea9a13
Jinseong <jinsung528@gmail.com>
2025-08-19 01:53:31 +0900

    [#251] Refactor: specification description 오타 수정

--- src/main/java/umc/GrowIT/Server/web/controller/specification/WithdrawalSpecification.java
+++ src/main/java/umc/GrowIT/Server/web/controller/specification/WithdrawalSpecification.java
@@ -11,7 +11,7 @@ import java.util.List;
 public interface WithdrawalSpecification {
 
     @GetMapping("/reasons")
-    @Operation(summary = "탈퇴이유 목록 조회 API", description = "회원탈퇴 시 선택할 수 있는 탈퇴사유 목록을 조회합니다.")
+    @Operation(summary = "탈퇴이유 목록 조회 API", description = "회원탈퇴 시 선택할 수 있는 탈퇴이유 목록을 조회합니다.")
     @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "⭕ SUCCESS")
     ApiResponse<List<WithdrawalResponseDTO.WithdrawalReasonDTO>> getWithdrawalReasons();
 }
