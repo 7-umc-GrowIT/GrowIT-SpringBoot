ba2740b8248d2de093a52279c9c3eb6a109aaa75
Jinseong <jinsung528@gmail.com>
2025-08-20 22:39:06 +0900

    [#256] Refactor: 기존 회원탈퇴 스케줄러 주석 처리

--- src/main/java/umc/GrowIT/Server/scheduler/Scheduler.java
+++ src/main/java/umc/GrowIT/Server/scheduler/Scheduler.java
@@ -44,31 +44,31 @@ public class Scheduler {
     }
 
     // 회원탈퇴 삭제 스케줄러
-    @Scheduled(cron = "0 0 0 * * ?") // 매일 자정 실행
-    @Transactional
-    public void deleteInactiveUsers() {
-        try {
-            // 기준일
-            int expirationDays = 30;
-
-            // 만료된 기준 날짜 계산 (현재 시간에서 30일 전)
-            LocalDateTime threshold = LocalDateTime.now().minusDays(expirationDays);
-
-            // 1. INACTIVE 상태이고 30일 이상 지난 사용자들의 RefreshToken ID 조회
-            List<Long> refreshTokenIds = userRepository.findRefreshTokenIdsForInactiveUsers(threshold);
-
-            // 2. INACTIVE 상태이고 30일 이상 지난 사용자 삭제
-            int deletedUserCount = userRepository.deleteByStatusAndUpdatedAtBefore(threshold);
-
-            // 3. 조회한 RefreshToken ID로 RefreshToken 삭제
-            if (!refreshTokenIds.isEmpty()) {
-                refreshTokenRepository.deleteByIds(refreshTokenIds);
-            }
-
-            log.info("[스케줄러] : inactive 상태이고 updated_at이 " + expirationDays + "일 지난 사용자 삭제");
-            log.info("[스케줄러] : 삭제된 사용자 수 : " + deletedUserCount);
-        } catch (Exception e) {
-            log.error("[스케줄러] : 사용자 삭제 실패", e);
-        }
-    }
+//    @Scheduled(cron = "0 0 0 * * ?") // 매일 자정 실행
+//    @Transactional
+//    public void deleteInactiveUsers() {
+//        try {
+//            // 기준일
+//            int expirationDays = 30;
+//
+//            // 만료된 기준 날짜 계산 (현재 시간에서 30일 전)
+//            LocalDateTime threshold = LocalDateTime.now().minusDays(expirationDays);
+//
+//            // 1. INACTIVE 상태이고 30일 이상 지난 사용자들의 RefreshToken ID 조회
+//            List<Long> refreshTokenIds = userRepository.findRefreshTokenIdsForInactiveUsers(threshold);
+//
+//            // 2. INACTIVE 상태이고 30일 이상 지난 사용자 삭제
+//            int deletedUserCount = userRepository.deleteByStatusAndUpdatedAtBefore(threshold);
+//
+//            // 3. 조회한 RefreshToken ID로 RefreshToken 삭제
+//            if (!refreshTokenIds.isEmpty()) {
+//                refreshTokenRepository.deleteByIds(refreshTokenIds);
+//            }
+//
+//            log.info("[스케줄러] : inactive 상태이고 updated_at이 " + expirationDays + "일 지난 사용자 삭제");
+//            log.info("[스케줄러] : 삭제된 사용자 수 : " + deletedUserCount);
+//        } catch (Exception e) {
+//            log.error("[스케줄러] : 사용자 삭제 실패", e);
+//        }
+//    }
 }
