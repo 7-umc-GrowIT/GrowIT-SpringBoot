1917bb7bababfa54a27f6936d302ba311c8ca422
벨라 <icomksh@gmail.com>
2025-08-24 13:08:16 +0900

    [#261] Feat: Revert to match main

--- src/main/java/umc/GrowIT/Server/service/userService/UserQueryService.java
+++ src/main/java/umc/GrowIT/Server/service/userService/UserQueryService.java
@@ -1,9 +1,5 @@
 package umc.GrowIT.Server.service.userService;
 
-import umc.GrowIT.Server.web.dto.UserDTO.UserResponseDTO;
-
 public interface UserQueryService {
     boolean isUserInactive(Long userId);
-
-    UserResponseDTO.MyPageDTO getMyPage(Long userId);
 }
--- src/main/java/umc/GrowIT/Server/service/userService/UserQueryServiceImpl.java
+++ src/main/java/umc/GrowIT/Server/service/userService/UserQueryServiceImpl.java
@@ -3,14 +3,9 @@ package umc.GrowIT.Server.service.userService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import umc.GrowIT.Server.apiPayload.code.status.ErrorStatus;
-import umc.GrowIT.Server.apiPayload.exception.GroHandler;
 import umc.GrowIT.Server.apiPayload.exception.UserHandler;
-import umc.GrowIT.Server.converter.UserConverter;
-import umc.GrowIT.Server.domain.Gro;
 import umc.GrowIT.Server.domain.User;
-import umc.GrowIT.Server.repository.GroRepository;
 import umc.GrowIT.Server.repository.UserRepository;
-import umc.GrowIT.Server.web.dto.UserDTO.UserResponseDTO;
 
 import static umc.GrowIT.Server.domain.enums.UserStatus.INACTIVE;
 
@@ -19,21 +14,11 @@ import static umc.GrowIT.Server.domain.enums.UserStatus.INACTIVE;
 public class UserQueryServiceImpl implements UserQueryService {
 
     private final UserRepository userRepository;
-    private final GroRepository groRepository;
 
-    @Override
     public boolean isUserInactive(Long userId) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new UserHandler(ErrorStatus.USER_NOT_FOUND));
 
         return user.getStatus() == INACTIVE;
-    }
-
-    @Override
-    public UserResponseDTO.MyPageDTO getMyPage(Long userId) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new UserHandler(ErrorStatus.USER_NOT_FOUND));
-
-        return UserConverter.toMyPageDTO(user);
     }
 }
