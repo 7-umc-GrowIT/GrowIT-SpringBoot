379a98ce7be2425d95ac9e3be46dae514fc574d9
벨라 <icomksh@gmail.com>
2025-08-28 10:50:48 +0900

    [#274] Refactor: 일기 dto 스키마 추가

--- src/main/java/umc/GrowIT/Server/web/dto/DiaryDTO/DiaryResponseDTO.java
+++ src/main/java/umc/GrowIT/Server/web/dto/DiaryDTO/DiaryResponseDTO.java
@@ -1,10 +1,10 @@
 package umc.GrowIT.Server.web.dto.DiaryDTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import umc.GrowIT.Server.domain.enums.UserChallengeType;
 import umc.GrowIT.Server.web.dto.ChallengeDTO.ChallengeResponseDTO;
 import umc.GrowIT.Server.web.dto.KeywordDTO.KeywordResponseDTO;
 
@@ -16,42 +16,59 @@ public class DiaryResponseDTO {
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(title = "작성된 일기 조회 response")
     public static class DiaryDateDTO{
+        @Schema(description = "일기 id", example = "1")
         Long diaryId;
-        LocalDate date;    //일기 최종 수정 날짜
+        @Schema(description = "일기 최종 수정 날짜")
+        LocalDate date;
     }
     @Builder
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(title = "일기 작성 날짜 조회 response")
     public static class DiaryDateListDTO{
-        List<DiaryDateDTO> diaryDateList;  //일기 작성 날짜들의 리스트
+        @Schema(description = "일기 작성 날짜들의 리스트")
+        List<DiaryDateDTO> diaryDateList;
+        @Schema(description = "리스트 사이즈")
         Integer listSize;
     }
     @Builder
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(title = "작성된 일기 내용 조회 response")
     public static class DiaryDTO{
+        @Schema(description = "일기 id", example = "1")
         Long diaryId;
+        @Schema(description = "일기 작성 내용", example = "오늘은 미라클 모닝을 해서 아침 8시에 학교에 도착했다. 그런데 강사님께서 ~")
         String content;
-        LocalDate date;    //일기 최종 수정 날짜
+        @Schema(description = "일기 최종 수정 날짜")
+        LocalDate date;
     }
     @Builder
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(title = "일기 모아보기 response")
     public static class DiaryListDTO {
+        @Schema(description = "일기 리스트")
         List<DiaryDTO> diaryList;
+        @Schema(description = "리스트 사이즈")
         Integer listSize;
     }
     @Builder
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(title = "일기 작성 response")
     public static class CreateDiaryResultDTO {
+        @Schema(description = "일기 id", example = "1")
         Long diaryId;
+        @Schema(description = "일기 작성 내용", example = "오늘은 미라클 모닝을 해서 아침 8시에 학교에 도착했다. 그런데 강사님께서 ~")
         String content;
+        @Schema(description = "일기 작성 날짜")
         LocalDate date;
     }
 
@@ -59,8 +76,11 @@ public class DiaryResponseDTO {
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(title = "일기 수정 response")
     public static class ModifyDiaryResultDTO {
+        @Schema(description = "일기 id", example = "1")
         Long diaryId;
+        @Schema(description = "일기 수정 내용", example = "오늘은 미라클 모닝을 해서 아침 8시에 학교에 도착했다. 그런데 강사님께서 ~")
         String content;
     }
 
@@ -68,7 +88,9 @@ public class DiaryResponseDTO {
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(title = "일기 삭제 response")
     public static class DeleteDiaryResultDTO {
+        @Schema(description = "일기 삭제 성공 메시지", example = "일기를 삭제했어요.")
         String message;
     }
 
@@ -76,27 +98,35 @@ public class DiaryResponseDTO {
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(title = "음성 대화 response")
     public static class VoiceChatResultDTO {
-        String chat;    //AI의 답변내용
+        @Schema(description = "AI의 답변 내용", example = "힘든 하루셨군요. 어떤 일이 있으셨나요?")
+        String chat;
     }
 
     @Builder
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(title = "일기 요약 response")
     public static class SummaryResultDTO {
+        @Schema(description = "일기 id", example = "1")
         Long diaryId;
+        @Schema(description = "일기 요약 내용", example = "오늘은 평소보다 더 차분한 하루를 보냈다. 아침에 일어나 창밖을 보니..")
         String content;
+        @Schema(description = "일기 작성 날짜")
         LocalDate date;
     }
 
-    // 챌린지 추천 응답 DTO
     @Getter
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(title = "챌린지 추천 response")
     public static class AnalyzedDiaryResponseDTO {
-        private List<KeywordResponseDTO.KeywordDTO> emotionKeywords; //감정키워드
-        private List<ChallengeResponseDTO.ChallengeDTO> recommendedChallenges; //추천챌린지
+        @Schema(description = "감정키워드")
+        private List<KeywordResponseDTO.KeywordDTO> emotionKeywords;
+        @Schema(description = "추천챌린지")
+        private List<ChallengeResponseDTO.ChallengeDTO> recommendedChallenges;
     }
 }
