443369a3e354451582e859d4ab209a4131474583
Jinseong <jinsung528@gmail.com>
2025-09-13 01:25:52 +0900

    [#373] Chore: 인증번호 발송 & 검증 API 엔드포인트 변경

--- src/main/java/umc/GrowIT/Server/web/controller/AuthController.java
+++ src/main/java/umc/GrowIT/Server/web/controller/AuthController.java
@@ -67,22 +67,22 @@ public class AuthController implements AuthSpecification {
     }
 
     @Override
-    @PostMapping("/email")
-    public ApiResponse<AuthResponseDTO.SendAuthEmailResponseDTO> sendAuthEmail(
+    @PostMapping("/email/send")
+    public ApiResponse<Void> sendAuthEmail(
             @RequestParam AuthType type,
             @RequestBody @Valid AuthRequestDTO.SendAuthEmailRequestDTO request) {
-        AuthResponseDTO.SendAuthEmailResponseDTO result = authService.sendAuthEmail(type, request);
 
-        return ApiResponse.onSuccess(result);
+        authService.sendAuthEmail(type, request);
+        return ApiResponse.onSuccess();
     }
 
     @Override
-    @PostMapping("/verification")
-    public ApiResponse<AuthResponseDTO.VerifyAuthCodeResponseDTO> verifyAuthCode(
+    @PostMapping("/email/verify")
+    public ApiResponse<Void> verifyAuthCode(
             @RequestBody @Valid AuthRequestDTO.VerifyAuthCodeRequestDTO request) {
-        AuthResponseDTO.VerifyAuthCodeResponseDTO result = authService.verifyAuthCode(request);
 
-        return ApiResponse.onSuccess(result);
+        authService.verifyAuthCode(request);
+        return ApiResponse.onSuccess();
     }
 
     @Override
