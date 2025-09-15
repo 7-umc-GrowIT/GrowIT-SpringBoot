16c110195c3ccbe0876e3236cdf3f53a02e42310
Jinseong <jinsung528@gmail.com>
2025-09-15 16:14:51 +0900

    [#388] Feat: PENDING 일기 삭제 스케줄러 구현

--- src/main/java/umc/GrowIT/Server/repository/DiaryRepository.java
+++ src/main/java/umc/GrowIT/Server/repository/DiaryRepository.java
@@ -34,6 +34,10 @@ public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
     boolean existsByUserIdAndDateAndStatus(Long userId, LocalDate date, DiaryStatus diaryStatus);
 
+    @Modifying
+    @Query("DELETE FROM Diary d WHERE d.status = :status")
+    int deleteByStatus(@Param("status") DiaryStatus status);
+
     // TODO 회원탈퇴 API 네이티브 삭제 메소드 - 추후 확인 필요
 //    @Modifying
 //    @Query(value = "DELETE FROM diary WHERE user_id = :userId", nativeQuery = true)
