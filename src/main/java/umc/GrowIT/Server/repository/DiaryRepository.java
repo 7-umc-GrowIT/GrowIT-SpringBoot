69ab52268a783e8e0fc46cc409ad9376e3a73c8e
Jinseong <jinsung528@gmail.com>
2025-09-19 23:36:56 +0900

    [#405] Fix: 일기 수정&삭제 API 조회 조건 수정

--- src/main/java/umc/GrowIT/Server/repository/DiaryRepository.java
+++ src/main/java/umc/GrowIT/Server/repository/DiaryRepository.java
@@ -47,6 +47,8 @@ public interface DiaryRepository extends JpaRepository<Diary, Long> {
     @Query("DELETE FROM Diary d WHERE d.status = :status")
     int deleteByStatus(@Param("status") DiaryStatus status);
 
+    Optional<Diary> findByIdAndUserId(Long diaryId, Long userId);
+
     // TODO 회원탈퇴 API 네이티브 삭제 메소드 - 추후 확인 필요
 //    @Modifying
 //    @Query(value = "DELETE FROM diary WHERE user_id = :userId", nativeQuery = true)
